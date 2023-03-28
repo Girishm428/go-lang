7640aacfeb372599aae2fb63ce603f1528ab3eba
Girish Reddy Madderi <girishm428@gmail.com>
2023-03-28 20:59:50 +0530

    handler push

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,11 +1,15 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/girishm428/go-lang/renders"
+)
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	RenderTemp(w, "home.page.tmpl")
+	renders.RenderTemp(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	RenderTemp(w, "about.page.tmpl")
+	renders.RenderTemp(w, "about.page.tmpl")
 }
