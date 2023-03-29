6b82a47261cc0d8fa1e5b4febd8e6ddbb2063919
Girishm428 <girishm428@gmail.com>
2023-03-29 12:43:25 +0530

    Update render.go

--- renders/render.go
+++ renders/render.go
@@ -7,7 +7,10 @@ import (
 )
 
 func RenderTemp(w http.ResponseWriter, tempName string) {
-	parsedfiles, _ := template.ParseFiles("D:/ShareX Captures/Go Lang/WorkSpace/templatesudemy/"+tempName, "D:/ShareX Captures/Go Lang/WorkSpace/templatesudemy/base.layout.tmpl")
+	parsedfiles, err1 := template.ParseFiles("D:/ShareX Captures/Go Lang/WorkSpace/templatesudemy/"+tempName, "D:/ShareX Captures/Go Lang/WorkSpace/templatesudemy/base.layout.tmpl")
+	if err1 != nil {
+		fmt.Println("Parsed files error1:", err1)
+	}
 	err := parsedfiles.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Parsed files error:", err)
