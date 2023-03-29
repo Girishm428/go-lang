a90a8a393f6fdf9e6abf15a488ca489e345e3b75
Girishm428 <girishm428@gmail.com>
2023-03-29 12:51:56 +0530

    Update handler.go

--- handlers/handler.go
+++ handlers/handler.go
@@ -10,6 +10,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	renders.RenderTemp(w, "home.page.tmpl")
 }
 
+// dont know where to find the erro
 func About(w http.ResponseWriter, r *http.Request) {
 	renders.RenderTemp(w, "about.page.tmpl")
 }
